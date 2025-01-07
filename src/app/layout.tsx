e01b994c1e9d35b3041c181aff8bad7ea2176788
kate-jiang <katejiang@pm.me>
2025-01-07 00:03:41 -0500

    use inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,19 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="dark-theme">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <div className="dots" />
         <Header />
