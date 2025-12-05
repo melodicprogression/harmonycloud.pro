ec2647e9aa76afde7795906e5d9b5071c27f3da8
Kate Jiang <katejiang@pm.me>
2025-12-05 01:14:43 -0500

    Enhance layout and styles
    - Added viewport configuration for improved mobile experience.
    - Updated background style in base.css for consistency.
    - Adjusted text shadow filter and added transform properties in sections.css for better visual effects.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 import "./css/globals.css";
 import { Header } from "./components";
@@ -6,6 +6,12 @@ import { Header } from "./components";
 export const metadata: Metadata = {
   title: "Harmony Cloud",
   description: "Sign up for the Harmony Cloud beta and learn more.",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#0b151e",
+  viewportFit: "cover",
+  colorScheme: "dark",
 };
 
 export default function RootLayout({
