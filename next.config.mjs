eb396ef856875b9590e126d97fcb4ba127880ecb
Kate Jiang <katejiang@pm.me>
2025-12-05 19:43:47 -0500

    add /appstore redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/appstore',
+        destination: 'https://apps.apple.com/us/app/harmony-cloud-pro/id6748590924',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
