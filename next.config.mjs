e5e6185eaaecc1bc8b4db687f8ce4cb4cd901a83
kate-jiang <katejiang@pm.me>
2026-01-06 14:40:34 -0500

    jen redirect

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         destination: 'https://apps.apple.com/us/app/harmony-cloud-pro/id6748590924',
         permanent: false,
       },
+      {
+        source: '/JEN2026',
+        destination: 'https://apps.apple.com/redeem?ctx=offercodes&id=6748590924&code=JEN2026',
+        permanent: false,
+      },
     ];
   },
 };
