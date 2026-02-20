a8538a5881a3d032f8c637dcfc2ef0204e83295b
kate-jiang <katejiang@pm.me>
2026-02-20 13:48:38 -0500

    embed img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,12 @@ import "./css/globals.css";
 import { Header } from "./components";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://harmonycloud.pro"),
   title: "Harmony Cloud",
   description: "Sign up for the Harmony Cloud beta and learn more.",
+  openGraph: {
+    type: "website",
+  },
 };
 
 export const viewport: Viewport = {
